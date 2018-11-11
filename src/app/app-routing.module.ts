3bbccd5f0430d657364f3896b1deabeb40295566
agaba kabunga <akabunga@belmotvillage.com>
2018-11-11 13:15:21 -0600

    add products list componet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './products/product-list.component';
 
 const routes: Routes = [
-  {path: 'product', component: ProductComponent}
+  {path: 'product', component: ProductComponent},
+  {path: 'products', component: ProductListComponent}
 ];
 
 @NgModule({
